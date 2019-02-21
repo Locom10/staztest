ec2172026958ed3393e35e09731bc032aff8482a
Hubert Koloszewski <hubert.koloszewski@billennium.com>
2019-02-21 13:12:52 +0100

    testy

--- tajemnicza-tablica.js
+++ tajemnicza-tablica.js
@@ -1,1 +1,9 @@
 console.log("tajemnicza-tablica.js")
+function myFunction() {
+    // var result = document.getElementById("myTable");
+    for (var i = 0, n = myTable.rows.length; i < n; i++) {
+        for (var j = 0, n = myTable.cells.length; j < n; j++)
+        var result = document.getElementById("myTable").rows[i].cells[j].innerHTML
+    }
+    console.log(myTable.rows.length)
+}
