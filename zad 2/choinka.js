3d43bba93ee785497d36d24e18fc741cbe67ad8a
Hubert Koloszewski <hubert.koloszewski@billennium.com>
2019-02-22 09:08:25 +0100

    choinka

--- zad 2/choinka.js
+++ zad 2/choinka.js
@@ -1,1 +1,16 @@
 console.log("choinka.js");
+function generuj(){
+    const pietra = document.getElementById("pietra").value;
+    const poziomy = document.getElementById("poziom").value;
+    const result = poziomy/pietra;
+    const div = document.getElementById('liczba');
+    div.innerHTML += result;
+    const tree = document.getElementById('tree');
+    tree.innerHTML += "*";
+    for (let i=0; i<result; i++) {
+        tree.innerHTML += "<br>";
+        for (let j=0; j<pietra; j++) {
+            tree.innerHTML += "<br>xD";
+        }
+    }
+}
